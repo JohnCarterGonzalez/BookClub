ccf1cefc77f0bb10805540f213207afa19ad5816
JohnCarterGonzalez <gjohncarter@protonmail.com>
2022-07-19 19:27:11 -0700

    Servive jf jfj
    ;lksajfd;lkasjd
    aslkdj;lsakjdf
    git commit -m Service

--- BookClub/src/main/java/com/johncarter/BookClub/services/UserService.java
+++ BookClub/src/main/java/com/johncarter/BookClub/services/UserService.java
@@ -3,6 +3,7 @@ package com.johncarter.BookClub.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
@@ -43,7 +44,23 @@ public class UserService {
 	* Login and Registration services
 	*/
 	public User reg(User nUser, BindingResult result) {
-		//Optional<User> potentialUser = urepo
+		Optional<User> potentialUser = urepo.findByEmail(nUser.getEmail());
+		if (potentialUser.isPresent()) {
+			result.rejectValue("email", "Unique", "Sorry this email is tied to an account that already exists");
+		}
+		if(!nUser.getPassword().equals(nUser.getConfirm()){
+			result.rejectValue("confirm", "The two passwords do not match!");
+		}
+
+		if(result.hasErrors()) {
+			return null;
+		}
+
+		String hashedpw = BCrypt.hashpw(nUser.getPassword(), BCrypt.gensalt());
+		nUser.setPassword(hashedpw);
+
+		nUser = urepo.save(nUser);
+		return nUser;
 	}
 	
 	
