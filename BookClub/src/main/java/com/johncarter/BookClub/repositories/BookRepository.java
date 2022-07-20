07c0b16e02d246d261d813ae78699675364e3e8e
JohnCarterGonzalez <gjohncarter@protonmail.com>
2022-07-19 17:48:52 -0700

    repositories Added the Repositories for both the Books and the Users

--- BookClub/src/main/java/com/johncarter/BookClub/repositories/BookRepository.java
+++ BookClub/src/main/java/com/johncarter/BookClub/repositories/BookRepository.java
@@ -1,5 +1,14 @@
 package com.johncarter.BookClub.repositories;
 
-public interface BookRepository {
+import java.util.List;
+
+import com.johncarter.BookClub.models.Book;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long>{
+	List<Book> findAll();
 
 }
--- BookClub/src/main/java/com/johncarter/BookClub/repositories/UserRepository.java
+++ BookClub/src/main/java/com/johncarter/BookClub/repositories/UserRepository.java
@@ -1,5 +1,15 @@
 package com.johncarter.BookClub.repositories;
 
-public interface UserRepository {
+import java.util.List;
+import java.util.Optional;
 
+import com.johncarter.BookClub.models.User;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{
+	List<User> findAll();
+	Optional<User> findByEmail(String emai);
 }
