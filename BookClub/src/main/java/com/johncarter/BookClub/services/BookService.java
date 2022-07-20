7c514dbcf6fd2b73e66cd200e1b175c70a45ebd0
JohnCarterGonzalez <gjohncarter@protonmail.com>
2022-07-19 18:19:53 -0700

    repos/services finished repos, added services for the app need to look at the model Books, table not connecting

--- BookClub/src/main/java/com/johncarter/BookClub/services/BookService.java
+++ BookClub/src/main/java/com/johncarter/BookClub/services/BookService.java
@@ -1,5 +1,41 @@
 package com.johncarter.BookClub.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.johncarter.BookClub.models.Book;
+import com.johncarter.BookClub.repositories.BookRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+	@Autowired
+	private BookRepository brepo;
+
+	public Book findbyId(Long id) {
+		Optional<Book> result = brepo.findById(id);
+		if(result.isPresent()) {
+			return result.get();
+		}
+		return null;
+	}
+
+	public List<Book> all() {
+		return brepo.findAll();
+	}
+	
+	public Book update(Book b) {
+		return brepo.save(b);
+	}
+
+	public Book create(Book b) {
+		return brepo.save(b);
+	}
+
+	public void delete(Book b) {
+		brepo.delete(b);
+	}
 
 }
